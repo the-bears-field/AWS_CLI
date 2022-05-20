8dbd17be5f037cda4da7d668d89c89198156884f
the-bears-field <thebearsfield+github@gmail.com>
2022-05-20 14:51:14 +0900

    start_ec2.sh編集。 EC2インスタンス起動後にElastic IPの取得と関連付けの処理を追加。 インデントを調整。
    参考:
    https://noisyspot.hatenablog.com/entry/20200314/1584182101
    https://docs.aws.amazon.com/cli/latest/reference/ec2/allocate-address.html
    https://docs.aws.amazon.com/cli/latest/reference/ec2/associate-address.html

--- shell_scripts/start_ec2.sh
+++ shell_scripts/start_ec2.sh
@@ -11,16 +11,16 @@ ec2_instance_state=`get_ec2_instance_state`
 
 # EC2インスタンスが稼働していれば終了。
 if [ ${ec2_instance_state} = "running" ]; then
-    echo "EC2インスタンス(インスタンスID: ${EC2_INSTANCE_ID})は起動済です。"
-    return
+  echo "EC2インスタンス(インスタンスID: ${EC2_INSTANCE_ID})は起動済です。"
+  return
 fi
 
 # EC2インスタンスを起動。
-ec2_instance_state=$(
-    aws ec2 start-instances \
-        --instance-ids $EC2_INSTANCE_ID \
-        --query StartingInstances[0].CurrentState.Name \
-        --output text \
+ec2_instance_state=$( \
+  aws ec2 start-instances \
+    --instance-ids $EC2_INSTANCE_ID \
+    --query StartingInstances[0].CurrentState.Name \
+    --output text \
 )
 
 echo "EC2インスタンス(インスタンスID: ${EC2_INSTANCE_ID})を起動中です。"
@@ -29,8 +29,35 @@ sleep 2s
 # EC2インスタンスの起動が確認できるまでループ処理。
 while [ ${ec2_instance_state} != "running" ]
 do
-    sleep 2s
-    ec2_instance_state=`get_ec2_instance_state`
+  sleep 2s
+  ec2_instance_state=`get_ec2_instance_state`
 done
 
 echo "EC2インスタンス(インスタンスID: ${EC2_INSTANCE_ID})を起動しました。"
+
+# Elastic IPアドレスとAllocation IDを取得
+set $( \
+  aws ec2 allocate-address \
+    --query [PublicIp,AllocationId] \
+    --output text \
+)
+
+# Elastic IPアドレスを変数に格納、envファイル追記
+elastic_ip_address=${1}
+echo "Elastic IP Address: ${elastic_ip_address}"
+sed -i "/# Elastic IP/a ELASTIC_IP_ADDRESS=${elastic_ip_address}" .env
+
+# Allocation IDを変数に格納、envファイル追記
+allocation_id=${2}
+echo "Allocation ID: ${allocation_id}"
+sed -i "/# Elastic IP/a ALLOCATION_ID=${allocation_id}" .env
+
+# ElasticIPアドレスとインスタンスとを関連付けし、Association IDを取得、envファイル追記
+association_id=$( \
+  aws ec2 associate-address \
+    --allocation-id ${allocation_id} \
+    --instance-id ${EC2_INSTANCE_ID} \
+    --query "AssociationId" \
+    --output text \
+) && echo "Association Id: ${association_id}" \
+&& sed -i "/# Elastic IP/a ASSOCIATION_ID=${association_id}" .env
