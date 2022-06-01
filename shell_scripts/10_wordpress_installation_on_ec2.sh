690a4ab31e4998bbbea66c6cee9c6f856c8fb82f
the-bears-field <thebearsfield+github@gmail.com>
2022-06-02 07:19:47 +0900

    EC2インスタンスにWordPress導入に必要な環境を構築した上で、 WordPressおよびWP-CLIインストールするshファイル作成。
    参考:
    https://qiita.com/softbase/items/b00b407d0a7a9fdf2a35
    Linux 標準教科書 ver.3.0.3 p.129
    https://wp-cli.org/ja/#%E3%82%A4%E3%83%B3%E3%82%B9%E3%83%88%E3%83%BC%E3%83%AB%E6%96%B9%E6%B3%95
    https://hapicode.com/server/aws-amazon-linux-extras.html

---
+++ shell_scripts/10_wordpress_installation_on_ec2.sh
@@ -0,0 +1,125 @@
+#!/bin/bash
+
+# 環境変数を再定義。
+eval "$(shdotenv)"
+
+# 共通関数ファイル読込。
+. /aws/shell_scripts/functions.sh
+
+# EC2インスタンスとRDSインスタンスの状態を取得し、
+# どちらか片方が起動していなければ処理を終了する。
+ec2_instance_state=`get_ec2_instance_state`
+rds_instance_state=`get_rds_instance_state`
+
+if [ $ec2_instance_state != "running" ] && [ $rds_instance_state != "available" ]; then
+  echo "EC2インスタンスとRDSインスタンスが起動していません。"
+  echo "EC2インスタンスとRDSインスタンスを起動して下さい。"
+  return
+elif [ $ec2_instance_state != "running" ]; then
+  echo "EC2インスタンスが起動していません。"
+  echo "EC2インスタンスを起動して下さい。"
+  return
+elif [ $rds_instance_state != "available" ]; then
+  echo "RDSインスタンスが起動していません。"
+  echo "RDSインスタンスを起動して下さい。"
+  return
+fi
+
+# EC2インスタンスへssh接続し、環境を構築した上でWordPressを導入。
+expect -c "
+set timeout 10
+spawn ssh -oStrictHostKeyChecking=no -i ${HOME}/.ssh/${SSH_KEY_NAME}.pem ec2-user@${ELASTIC_IP_ADDRESS}
+expect {
+  -glob \"ec2-user@*\" {
+    log_user 1
+    send \"sudo amazon-linux-extras install -y php7.2\n\"
+  }
+}
+
+expect {
+  -glob \"ec2-user@*\" {
+    sleep 2
+    log_user 1
+    send \"sudo yum -y install php php-mbstring\n\"
+  }
+}
+
+expect {
+  -glob \"ec2-user@*\" {
+    sleep 2
+    log_user 1
+    send \"wget https://ja.wordpress.org/latest-ja.tar.gz\n\"
+  }
+}
+
+expect {
+  -glob \"ec2-user@*\" {
+    sleep 2
+    log_user 1
+    send \"tar xzf latest-ja.tar.gz\n\"
+  }
+}
+
+expect {
+  -glob \"ec2-user@*\" {
+    sleep 2
+    log_user 1
+    send \"sudo cp -r wordpress/* /var/www/html/\n\"
+  }
+}
+
+expect {
+  -glob \"ec2-user@*\" {
+    sleep 2
+    log_user 1
+    send \"sudo chown apache:apache /var/www/html/ -R\n\"
+  }
+}
+
+expect {
+  -glob \"ec2-user@*\" {
+    sleep 2
+    log_user 1
+    send \"sudo systemctl restart httpd.service\n\"
+  }
+}
+
+expect {
+  -glob \"ec2-user@*\" {
+    sleep 2
+    log_user 1
+    send \"curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar\n\"
+  }
+}
+
+expect {
+  -glob \"ec2-user@*\" {
+    sleep 2
+    log_user 1
+    send \"php wp-cli.phar --info\n\"
+  }
+}
+
+expect {
+  -glob \"ec2-user@*\" {
+    sleep 2
+    log_user 1
+    send \"chmod +x wp-cli.phar\n\"
+  }
+}
+
+expect {
+  -glob \"ec2-user@*\" {
+    sleep 2
+    log_user 1
+    send \"sudo mv wp-cli.phar /usr/local/bin/wp\n\"
+  }
+}
+
+expect {
+  -regexp "\n.*\r" {
+    send \"exit\n\"
+    exit 0
+  }
+}
+"
